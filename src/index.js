0dace143e07f03c55943da36c0fedf555080300a
Yohanpy <northpark0@gmail.com>
2020-10-02 21:35:02 +0300

    feat: add min and max functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    } else {
+        var minResult = array[0];
+        for (var i = 0; i < array.length; i++) {
+        if (array[i] < minResult) {
+            minResult = array[i];
+        }
+        }
+    }
+    return minResult;
 }
 
+
 exports.max = function max (array) {
-  return 0;
+    if (!Array.isArray(array) || !array.length) {
+        return 0;
+    } else {
+        var maxResult = array[0];
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] > maxResult) {
+                maxResult = array[i];
+            }
+        }
+    }
+    return maxResult;
 }
 
 exports.avg = function avg (array) {
